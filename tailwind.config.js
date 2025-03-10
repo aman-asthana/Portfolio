d10338c95f1e45c8b9839097ec81e223bf45ee4f
Aman Asthana <amanasthana431@gmail.com>
2025-03-11 00:51:43 +0530

    tailwindCSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,14 @@ export default {
   theme: {
     extend: {
       animation: {
-        gradient: 'gradient 8s linear infinite',
+        gradient: 'gradient 8s ease infinite',
+        'gradient-slow': 'gradient 12s ease infinite',
+        'gradient-fast': 'gradient 6s ease infinite',
       },
       keyframes: {
         gradient: {
-          '0%': { backgroundPosition: '0% 50%' },
+          '0%, 100%': { backgroundPosition: '0% 50%' },
           '50%': { backgroundPosition: '100% 50%' },
-          '100%': { backgroundPosition: '0% 50%' },
         },
       },
       backgroundImage: {
